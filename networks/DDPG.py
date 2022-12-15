57f7eee610f10f360d7c9efdaddd30a13e2f099b
retinfai <aferetipama@gmail.com>
2022-12-15 14:03:34 +1300

    refactor: add gpu device support

--- networks/DDPG.py
+++ networks/DDPG.py
@@ -1,5 +1,6 @@
 
 import torch
+import torch.nn as nn
 import numpy as np
 import copy
 
@@ -10,33 +11,36 @@ class DDPG:
                  actor_network,
                  critic_network,
                  gamma,
-                 tau
+                 tau,
+                 device
                  ):
 
-        self.actor_net = actor_network
-        self.targ_actor_net = copy.deepcopy(actor_network)
+        self.actor_net: nn.Module = actor_network.to(device)
+        self.targ_actor_net: nn.Module = copy.deepcopy(actor_network)
 
-        self.critic_net = critic_network
-        self.targ_critic_net = copy.deepcopy(critic_network)
+        self.critic_net: nn.Module = critic_network.to(device)
+        self.targ_critic_net: nn.Module = copy.deepcopy(critic_network)
 
         self.gamma = gamma
         self.tau = tau
+
+        self.device = device
 
     def forward(self, observation):
         return self.actor_net.forward(observation)
 
     def learn(self, experiences):
 
-        batch_size = len(experiences)
-
         states, actions, rewards, next_states, dones = experiences
 
+        batch_size = len(states)
+
         # Convert into tensor
-        states = torch.FloatTensor(np.asarray(states))
-        actions = torch.FloatTensor(np.asarray(actions))
-        rewards = torch.FloatTensor(rewards)
-        next_states = torch.FloatTensor(np.asarray(next_states))
-        dones = torch.LongTensor(dones)
+        states = torch.FloatTensor(np.asarray(states)).to(self.device)
+        actions = torch.FloatTensor(np.asarray(actions)).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.FloatTensor(np.asarray(next_states)).to(self.device)
+        dones = torch.LongTensor(dones).to(self.device)
 
         # Reshape to batch_size x whatever
         rewards = rewards.unsqueeze(0).reshape(batch_size, 1)
@@ -45,7 +49,7 @@ class DDPG:
         # We do not want the gradients calculated for any of the target old_networks, we manually update the parameters
         with torch.no_grad():
 
-            next_actions = self.targ_actor_net(next_states)
+            next_actions = self.targ_actor_net(next_states).to(self.device)
             next_q_values = self.targ_critic_net(next_states, next_actions)
 
             q_target = rewards + self.gamma * (1 - dones) * next_q_values
@@ -60,7 +64,7 @@ class DDPG:
         self.critic_net.optimiser.step()
 
         # Update the Actor Network
-        actor_q = self.critic_net(states, self.actor_net(states))
+        actor_q = self.critic_net(states, self.actor_net(states).to(self.device))
         actor_loss = -actor_q.mean()
 
         self.actor_net.optimiser.zero_grad()
