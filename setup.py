cb7c36220859d6374f7213e59c4e8d9d8bbb7720
retinfai <aferetipama@gmail.com>
2022-12-19 12:00:05 +1300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cares_reinforcement_learning',
+    version='0.0',
+    packages=['util', 'networks'],
+    url='',
+    license='',
+    author='CARES',
+    author_email='cares@aucklanduni.ac.nz',
+    description='CARES reinforcement learning package'
+)
