f4e86913e9add74f6701a8e99a5695d5edfdbcaa
david <davidvalenciaredrovan@gmail.com>
2023-06-16 18:25:03 +1200

    initial NASA td3

--- cares_reinforcement_learning/algorithm/policy/NASA-TD3.py
+++ cares_reinforcement_learning/algorithm/policy/NASA-TD3.py
@@ -19,34 +19,42 @@ from networks import EPDM  # Deterministic Ensemble
 
 class NASA_TD3:
 
-    #def __init__(self, latent_size, action_num, device, k):
-    def __init__(self, actor_network, critic_network, encoder_net, decoder_net, action_num, device, k):
+    def __init__(self,
+                 encoder_network,
+                 decoder_network,
+                 actor_network,
+                 critic_network,
+                 action_num,
+                 latent_size,
+                 device):
 
-        self.latent_size = latent_size
+        self.gamma = 0.99
+        self.tau   = 0.005
+        self.ensemble_size = 10
+        self.latent_size   = latent_size
+
+        self.learn_counter      = 0
+        self.policy_update_freq = 2
+
         self.action_num  = action_num
         self.device      = device
 
+        self.encoder = encoder_network.to(device)
+        self.decoder = decoder_network.to(device)
+        self.actor   = actor_network.to(device)
+        self.critic  = critic_network.to(device)
 
-        # self.k = k * 3 # number of stack frames*3, 3 because I am using 3CH images
-        #
-        # self.encoder = Encoder(latent_dim=self.latent_size, k=self.k).to(self.device)
-        # self.decoder = Decoder(latent_dim=self.latent_size, k=self.k).to(self.device)
-        #
-        # # TODO note that for gripper I need to inject the goal angle so the laten_size become latent_size+1
-        # self.actor  = Actor(self.latent_size, self.action_num, self.encoder).to(self.device)
-        # self.critic = Critic(self.latent_size, self.action_num, self.encoder).to(self.device)
-        #
-        # # TODO test what happen with copy.deepcopy(self.actor_net).to(device)
-        # self.actor_target  = Actor(self.latent_size, self.action_num, self.encoder).to(self.device)
-        # self.critic_target = Critic(self.latent_size, self.action_num, self.encoder).to(self.device)
-        #
-        # self.critic_target.load_state_dict(self.critic.state_dict())
-        # self.actor_target.load_state_dict(self.actor.state_dict())
-        #
-        # self.epm = nn.ModuleList()
-        # networks = [EPDM(self.latent_size, self.action_num) for _ in range(self.ensemble_size)]
-        # self.epm.extend(networks)
-        # self.epm.to(self.device)
+        self.actor_target  = copy.deepcopy(self.actor).to(device)
+        self.critic_target = copy.deepcopy(self.critic).to(device)
+
+        # Necessary for make the same encoder in the whole algorithm
+        self.actor_target.encoder_net = self.encoder
+        self.critic_target.encoder_net = self.encoder
+
+        self.epm = nn.ModuleList()
+        networks = [EPDM(self.latent_size, self.action_num) for _ in range(self.ensemble_size)]
+        self.epm.extend(networks)
+        self.epm.to(self.device)
 
         lr_actor   = 1e-4
         lr_critic  = 1e-3
@@ -62,10 +70,18 @@ class NASA_TD3:
         w_decay_epm = 1e-3
         self.epm_optimizers = [torch.optim.Adam(self.epm[i].parameters(), lr=lr_epm, weight_decay=w_decay_epm) for i in range(self.ensemble_size)]
 
-        self.gamma = 0.99
-        self.tau   = 0.005
-        self.ensemble_size = 10
-
-        self.learn_counter      = 0
-        self.policy_update_freq = 2
+        def select_action_from_policy(self, state, evaluation=False, noise_scale=0.1):
+            self.actor.eval()
+            with torch.no_grad():
+                state_tensor = torch.FloatTensor(state).to(self.device)
+                state_tensor = state_tensor.unsqueeze(0)
+                action = self.actor(state_tensor)
+                action = action.cpu().data.numpy().flatten()
+                if not evaluation:
+                    # this is part the TD3 too, add noise to the action
+                    noise = np.random.normal(0, scale=noise_scale, size=self.action_num)
+                    action = action + noise
+                    action = np.clip(action, -1, 1)
+            self.actor.train()
+            return action
 
