a45a8272b443871b080323e2ee5a72ed4bec3c44
david <davidvalenciaredrovan@gmail.com>
2023-10-16 16:17:44 +1300

    fix new network conf TD3

--- cares_reinforcement_learning/networks/TD3/Actor.py
+++ cares_reinforcement_learning/networks/TD3/Actor.py
@@ -1,7 +1,7 @@
-import torch
+
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
+
 
 class Actor(nn.Module):
     def __init__(self, observation_size, num_actions, learning_rate):
@@ -9,14 +9,17 @@ class Actor(nn.Module):
 
         self.hidden_size = [1024, 1024]
 
-        self.h_linear_1 = nn.Linear(in_features=observation_size, out_features=self.hidden_size[0])
-        self.h_linear_2 = nn.Linear(in_features=self.hidden_size[0], out_features=self.hidden_size[1])
-        self.h_linear_3 = nn.Linear(in_features=self.hidden_size[1], out_features=num_actions)
+        self.act_net = nn.Sequential(
+            nn.Linear(observation_size, self.hidden_size[0]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[0], self.hidden_size[1]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[1], num_actions),
+            nn.Tanh()
+        )
 
         self.optimiser = optim.Adam(self.parameters(), lr=learning_rate)
 
     def forward(self, state):
-        x = F.relu(self.h_linear_1(state))
-        x = F.relu(self.h_linear_2(x))
-        x = torch.tanh(self.h_linear_3(x))
-        return x
+        output   = self.act_net(state)
+        return output
