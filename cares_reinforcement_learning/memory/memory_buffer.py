96909f7d32fa1ae773bd05687350b27cfcffdae7
david <davidvalenciaredrovan@gmail.com>
2024-02-21 09:42:46 +1300

    fix memory class issue

--- cares_reinforcement_learning/memory/memory_buffer.py
+++ cares_reinforcement_learning/memory/memory_buffer.py
@@ -1,7 +1,5 @@
-import logging
 import random
 from collections import deque
-import numpy as np
 
 
 class MemoryBuffer:
@@ -14,10 +12,7 @@ class MemoryBuffer:
     def sample(self, batch_size):
         batch_size = min(batch_size, len(self.buffer))
         experience_batch = random.sample(self.buffer, batch_size)
-        # states, actions, rewards, next_states, dones = zip(*experience_batch)
-        # return states, actions, rewards, next_states, dones
         transposed_batch = zip(*experience_batch)
-        # return [component for component in transposed_batch]
         return transposed_batch
 
     def flush(self):
@@ -26,49 +21,3 @@ class MemoryBuffer:
         )
         self.buffer.clear()
         return states, actions, rewards, next_states, dones, log_probs
-
-
-# #----------------------------------------------------------------------------------------
-# #----------------------------------------------------------------------------------------
-
-# class MemoryBuffer:
-#     def __init__(self, max_capacity=int(1e6)):
-#         self.max_size = max_capacity
-#         self.ptr = 0
-#         self.size = 0
-#         self.state = None
-#
-#     def add(self, state, action, reward, next_state, done):
-#
-#         if self.state is None:
-#             # Infer dimensions from the inputs to avoid passing them as arg
-#             state_dim =  state.shape[0]
-#             action_dim = action.shape[0]
-
-#             # Initialize arrays
-#             self.state = np.empty((self.max_size, *state_dim))
-#             self.action = np.zeros((self.max_size, action_dim))
-#             self.next_state = np.zeros((self.max_size, state_dim))
-#             self.reward = np.zeros((self.max_size))
-#             self.done = np.zeros((self.max_size))
-#
-#         # Add the experience to the buffer
-#         self.state[self.ptr] = state
-#         self.action[self.ptr] = action
-#         self.next_state[self.ptr] = next_state
-#         self.reward[self.ptr] = reward
-#         self.done[self.ptr] = done
-#
-#         self.ptr = (self.ptr + 1) % self.max_size
-#         self.size = min(self.size + 1, self.max_size)
-#
-#     def sample(self, batch_size):
-#         #batch_size = min(batch_size, self.size) revisa luego
-#         ind = np.random.randint(0, self.size, size=batch_size)
-#         return (
-#             self.state[ind],
-#             self.action[ind],
-#             self.reward[ind],
-#             self.next_state[ind],
-#             self.done[ind],
-#         )
