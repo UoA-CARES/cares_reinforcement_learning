b88ee892b89b8d202402699bb80b79a1be0a6c23
tonyq <tingq111011@gmail.com>
2024-02-23 21:55:42 +1300

    naming conventions

--- cares_reinforcement_learning/memory/memory_buffer.py
+++ cares_reinforcement_learning/memory/memory_buffer.py
@@ -33,19 +33,17 @@ class MemoryBuffer:
         For MBRL
         """
         batch_size = min(batch_size, len(self.buffer))
-        max = len(self.buffer)
-        idxs = np.random.randint(0, (max - 1), size=batch_size)
+        max_length = len(self.buffer)
+        idxs = np.random.randint(0, (max_length - 1), size=batch_size)
         # A list of tuples
         experience_batch = [
             self.buffer[i]
             + (
-                self.buffer[i + 1][1],
-                self.buffer[i + 1][2],
+                self.buffer[i+1][1],
+                self.buffer[i+1][2],
             )
             for i in idxs
         ]
-
-        # experience_batch = random.sample(self.buffer, batch_size)
         transposed_batch = zip(*experience_batch)
         return transposed_batch
 
@@ -64,18 +62,15 @@ class MemoryBuffer:
         Compute the statisitics for world model normalization.
         :return:
         """
-
-        states = [tuple[0] for tuple in self.buffer]
-        next_states = [tuple[3] for tuple in self.buffer]
+        states = [trans[0] for trans in self.buffer]
+        next_states = [trans[3] for trans in self.buffer]
         states = np.array(states)
         next_states = np.array(next_states)
         delta = next_states - states
-
         statistics = {
             "ob_mean": np.mean(states, axis=0) + 0.0001,
             "ob_std": np.std(states, axis=0) + 0.0001,
             "delta_mean": np.mean(delta, axis=0) + 0.0001,
             "delta_std": np.std(delta, axis=0) + 0.0001,
         }
-
         return statistics
