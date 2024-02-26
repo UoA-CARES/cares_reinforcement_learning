cfff508122b7299a751435ad71fe0edfc76da905
david <davidvalenciaredrovan@gmail.com>
2024-02-27 10:03:28 +1300

    Resolve formatting issues

--- cares_reinforcement_learning/memory/memory_buffer.py
+++ cares_reinforcement_learning/memory/memory_buffer.py
@@ -1,13 +1,14 @@
 import random
 from collections import deque
 
+
 class MemoryBuffer:
     def __init__(self, max_capacity=int(1e6)):
         self.buffer = deque(maxlen=max_capacity)
 
     def __len__(self):
         return len(self.buffer)
-    
+
     def add(self, *experience):
         self.buffer.append(experience)
 
