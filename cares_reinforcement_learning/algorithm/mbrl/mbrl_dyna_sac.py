7e8aa553dd59abe8aa48f2686b86caaf9981bdcb
tonyq <tingq111011@gmail.com>
2024-03-05 12:42:23 +1300

    For passing linting

--- cares_reinforcement_learning/algorithm/mbrl/mbrl_dyna_sac.py
+++ cares_reinforcement_learning/algorithm/mbrl/mbrl_dyna_sac.py
@@ -251,6 +251,7 @@ class MBRL_DYNA_SAC:
             # On-policy generating.Temporary results shows no diff with random.
             # pred_acts, _, _ = self.actor_net.sample(pred_state)
             # rand_acts = np.random.uniform(-1,1,(self.num_samples, self.action_num))
+
             rand_acts = np.random.uniform(-1, 1, (pred_state.shape[0], self.action_num))
             pred_acts = torch.FloatTensor(rand_acts).to(self.device)
             # pred_acts = pred_acts.repeat(repeats=next_states.shape[0])
