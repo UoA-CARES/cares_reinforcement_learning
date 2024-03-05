02390c43aaa436b255282646cfeb0d6558ba7077
tonyq <tingq111011@gmail.com>
2024-03-05 16:05:35 +1300

    1. Takes in a learning rate for world model. 2. Comment out the fig_manager for now.

--- cares_reinforcement_learning/networks/World_Models/ensemble_integrated.py
+++ cares_reinforcement_learning/networks/World_Models/ensemble_integrated.py
@@ -107,7 +107,7 @@ class Ensemble_World_Reward:
     """
 
     def __init__(
-        self, observation_size, num_actions, num_models, hidden_size=128, lr=0.001
+        self, observation_size, num_actions, num_models, lr, hidden_size=128,
     ):
         self.device = None
         self.num_models = num_models
