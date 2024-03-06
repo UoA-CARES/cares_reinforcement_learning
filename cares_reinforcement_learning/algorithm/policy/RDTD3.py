c573c1d824543525d3fda81031934e365f568d0a
retinfai <aferetipama@gmail.com>
2024-03-07 12:22:51 +1300

    Fix save and load models function

--- cares_reinforcement_learning/algorithm/policy/RDTD3.py
+++ cares_reinforcement_learning/algorithm/policy/RDTD3.py
@@ -186,16 +186,20 @@ class RDTD3:
 
         return info
 
-    def save_models(self, filename):
-        dir_exists = os.path.exists("models")
+    def save_models(self, filename, filepath="models"):
+        path = f"{filepath}/models" if filepath != "models" else filepath
+        dir_exists = os.path.exists(path)
 
         if not dir_exists:
-            os.makedirs("models")
-        torch.save(self.actor_net.state_dict(), f'models/{filename}_actor.pht')
-        torch.save(self.critic_net.state_dict(), f'models/{filename}_critic.pht')
-        logging.info("models has been loaded...")
+            os.makedirs(path)
 
-    def load_models(self, filename):
-        self.actor_net.load_state_dict(torch.load(f'models/{filename}_actor.pht'))
-        self.critic_net.load_state_dict(torch.load(f'models/{filename}_critic.pht'))
+        torch.save(self.actor_net.state_dict(), f"{path}/{filename}_actor.pht")
+        torch.save(self.critic_net.state_dict(), f"{path}/{filename}_critic.pht")
+        logging.info("models has been saved...")
+
+    def load_models(self, filepath, filename):
+        path = f"{filepath}/models" if filepath != "models" else filepath
+
+        self.actor_net.load_state_dict(torch.load(f"{path}/{filename}_actor.pht"))
+        self.critic_net.load_state_dict(torch.load(f"{path}/{filename}_critic.pht"))
         logging.info("models has been loaded...")
