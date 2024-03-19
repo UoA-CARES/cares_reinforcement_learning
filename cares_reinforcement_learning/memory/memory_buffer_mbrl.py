987283cc10423755bd67ed18de1aaa16c90b46d8
tonyq <tingq111011@gmail.com>
2024-03-20 10:26:34 +1300

    MemoryBufferMBRL inherit the MemoryBuffer.

--- cares_reinforcement_learning/memory/memory_buffer_mbrl.py
+++ cares_reinforcement_learning/memory/memory_buffer_mbrl.py
@@ -1,9 +1,9 @@
 import random
-from collections import deque
 import numpy as np
+from cares_reinforcement_learning.memory import MemoryBuffer
 
 
-class MemoryBufferMBRL:
+class MemoryBufferMBRL(MemoryBuffer):
     """
     A memory buffer to temporarily store transitions. It is like a transition
     pool. Only off-policy algorithms needs random sampling this buffer.
@@ -11,47 +11,23 @@ class MemoryBufferMBRL:
     """
 
     def __init__(self, max_capacity=int(1e6)):
-        self.buffer = deque(maxlen=max_capacity)
-
-    def __len__(self):
-        return len(self.buffer)
-
-    def add(self, *experience):
-        """
-        Add new transitions into the buffer. Be aware of the sequence. Should
-        be consistent with how it is sampled.
-
-        Example:
-
-        memory.add(state, action, reward, next_state, done)
-
-        :param experience: a tuple of a transition.
-        """
-        self.buffer.append(experience)
-
-    def sample(self, batch_size):
-        """
-        Sample for training the agent.
-
-        :param batch_size: It is pre-set by training configuration file.
-        """
-        batch_size = min(batch_size, len(self.buffer))
-        experience_batch = random.sample(self.buffer, batch_size)
-        transposed_batch = zip(*experience_batch)
-        return transposed_batch
+        super().__init__(max_capacity)
 
     def sample_consecutive(self, batch_size):
         """
         For training MBRL to predict rewards. The right next transition is
         sampled as well. WHEN THE BUFFER IS NOT SHUFFLED.
 
+        It is not overriding the original sample() due to sample() can be used
+        for normal training, and the current sample_consecutive() is slower.
+
         (State, action, reward, next_state, next_action, next_reard)
 
         """
         max_length = len(self.buffer) - 1
+        candi_indices = list(range(max_length))
         batch_size = min(batch_size, max_length)
         # A list of candidate indices includes all indices.
-        candi_indices = list(range(max_length))
         sampled_indices = []  # randomly sampled indices that is okay.
         # In this way, the sampling time depends on the batch size rather than buffer size.
         first_sample = True  # Not check duplicate for first time sample.
@@ -85,23 +61,6 @@ class MemoryBufferMBRL:
         ]
         transposed_batch = zip(*experience_batch)
         return transposed_batch
-
-    def flush(self):
-        """
-        For on-policy PPO. Output all collected trajectories.
-        And clear the buffer.
-
-        :return:
-        """
-        experience = list(zip(*self.buffer))
-        self.buffer.clear()
-        return experience
-
-    def clear(self):
-        """
-        Clear the buffer.
-        """
-        self.buffer.clear()
 
     def get_statistics(self):
         """
