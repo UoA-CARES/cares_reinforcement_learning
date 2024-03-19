389a5ef900eb98a6daf1d7074bfa347e7b01e669
tonyq <tingq111011@gmail.com>
2024-03-19 18:10:30 +1300

    change Original memory_buffer.py back to original.

--- cares_reinforcement_learning/memory/memory_buffer.py
+++ cares_reinforcement_learning/memory/memory_buffer.py
@@ -1,6 +1,5 @@
 import random
 from collections import deque
-import numpy as np
 
 
 class MemoryBuffer:
@@ -37,52 +36,6 @@ class MemoryBuffer:
         """
         batch_size = min(batch_size, len(self.buffer))
         experience_batch = random.sample(self.buffer, batch_size)
-        transposed_batch = zip(*experience_batch)
-        return transposed_batch
-
-    def sample_consecutive(self, batch_size):
-        """
-        For training MBRL to predict rewards. The right next transition is
-        sampled as well. WHEN THE BUFFER IS NOT SHUFFLED.
-
-        (State, action, reward, next_state, next_action, next_reard)
-
-        """
-        max_length = len(self.buffer) - 1
-        batch_size = min(batch_size, max_length)
-        # A list of candidate indices includes all indices.
-        candi_indices = list(range(max_length))
-        sampled_indices = []  # randomly sampled indices that is okay.
-        # In this way, the sampling time depends on the batch size rather than buffer size.
-        first_sample = True  # Not check duplicate for first time sample.
-        while True:
-            # Sample size based on how many still needed.
-            idxs = random.sample(candi_indices, batch_size - len(sampled_indices))
-            for i in idxs:
-                # Check if it is already sampled.
-                already_sampled = False
-                # Only check if it is not first time in the while loop.
-                if not first_sample:
-                    # compare with each item in the sampled.
-                    for j in sampled_indices:
-                        if j == i:
-                            already_sampled = True
-                if (self.buffer[i][4] is False) and (not already_sampled):
-                    sampled_indices.append(i)
-                if len(sampled_indices) == batch_size:
-                    break
-            first_sample = False
-            if len(sampled_indices) == batch_size:
-                break
-        # Form the sampled data batch
-        experience_batch = [
-            self.buffer[i]
-            + (
-                self.buffer[i + 1][1],
-                self.buffer[i + 1][2],
-            )
-            for i in sampled_indices
-        ]
         transposed_batch = zip(*experience_batch)
         return transposed_batch
 
