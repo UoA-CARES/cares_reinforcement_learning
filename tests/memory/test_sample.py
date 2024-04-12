27dff9dd149702276764195abfb51071df12d785
Henry Williams <HenryAMWilliams@gmail.com>
2024-04-12 15:35:20 +1200

    Memory Sample Consecutive (#144)
    * start of LA3TD3
    
    * pushed sample into algorithms
    
    * initial actor prio b uffer
    
    * Adding LA3PD - updating buffer to have priority sample, inverse priority sample, and uniform sample
    
    * LA3PTD3 testing
    
    * minor comments
    
    * MAPERTD3
    
    * weight actor update
    
    * fixed actor and critic
    
    * MAPERTD3 functional test
    
    * updated alpha for LAP
    
    * updated unfirom for CTD4
    
    * Updated Memory to force format of memory buffer state, etc
    
    * start of consect
    
    * Added sample consecutive and pytest
    
    * Re added consistent insertion test
    
    * improved test functions
    
    * updated and improved tests for memory
    
    * commenting + refactor of variable names
    
    * updated names - and docs

--- tests/memory/test_sample.py
+++ tests/memory/test_sample.py
@@ -1,14 +1,14 @@
 from memory import memory_buffer, memory_buffer_1e6
 
 
-def test_sample(memory_buffer):
+def test_sample(memory_buffer_1e6):
 
     for i in range(4):
-        memory_buffer.add(i, i, i, i, False, 0.5 * i)
+        memory_buffer_1e6.add(i, i, i, i, False, 0.5 * i)
 
     batch_size = 3
     states, actions, rewards, next_states, dones, log_probs, ind = (
-        memory_buffer.sample_uniform(batch_size)
+        memory_buffer_1e6.sample_uniform(batch_size)
     )
 
     assert (
@@ -23,7 +23,7 @@ def test_sample(memory_buffer):
     )
 
     states, actions, rewards, next_states, dones, log_probs, ind, weights = (
-        memory_buffer.sample_inverse_priority(batch_size)
+        memory_buffer_1e6.sample_inverse_priority(batch_size)
     )
 
     assert (
@@ -39,7 +39,7 @@ def test_sample(memory_buffer):
     )
 
     states, actions, rewards, next_states, dones, log_probs, ind, weights = (
-        memory_buffer.sample_priority(batch_size)
+        memory_buffer_1e6.sample_priority(batch_size)
     )
 
     assert (
@@ -55,46 +55,95 @@ def test_sample(memory_buffer):
     )
 
 
-def test_sample_values(memory_buffer):
-    memory_buffer.add(0.5, 1.0, 1.5, 2.0, False, 0.5)
+def test_sample_values(memory_buffer_1e6):
+    for i in range(10):
+        memory_buffer_1e6.add(i, i, i, i, i % 2, i)
 
     states, actions, rewards, next_states, dones, log_probs, ind = (
-        memory_buffer.sample_uniform(1)
+        memory_buffer_1e6.sample_uniform(1)
     )
 
-    assert states == [0.5]
-    assert actions == [1.0]
-    assert rewards == [1.5]
-    assert next_states == [2.0]
-    assert dones == [False]
-    assert log_probs == [0.5]
-    assert ind == [0]
+    value = ind[0]
+    assert states == [value]
+    assert actions == [value]
+    assert rewards == [value]
+    assert next_states == [value]
+    assert dones == [value % 2]
+    assert log_probs == [value]
+    assert ind == [value]
+
+
+def test_sample_priority_values(memory_buffer_1e6):
+    for i in range(10):
+        memory_buffer_1e6.add(i, i, i, i, i % 2, i)
 
     states, actions, rewards, next_states, dones, log_probs, ind, weights = (
-        memory_buffer.sample_priority(1)
+        memory_buffer_1e6.sample_priority(1)
     )
 
-    assert states == [0.5]
-    assert actions == [1.0]
-    assert rewards == [1.5]
-    assert next_states == [2.0]
-    assert dones == [False]
-    assert log_probs == [0.5]
-    assert ind == [0]
+    value = ind[0]
+    assert states == [value]
+    assert actions == [value]
+    assert rewards == [value]
+    assert next_states == [value]
+    assert dones == [value % 2]
+    assert log_probs == [value]
+    assert ind == [value]
     assert weights == [1.0]
 
+
+def test_sample_inverse_sample_values(memory_buffer_1e6):
+    size = 10
+    for i in range(size):
+        memory_buffer_1e6.add(i, i, i, i, i % 2, i)
+
     states, actions, rewards, next_states, dones, log_probs, ind, weights = (
-        memory_buffer.sample_inverse_priority(1)
+        memory_buffer_1e6.sample_inverse_priority(1)
     )
 
-    assert states == [0.5]
-    assert actions == [1.0]
-    assert rewards == [1.5]
-    assert next_states == [2.0]
-    assert dones == [False]
-    assert log_probs == [0.5]
-    assert ind == [0]
-    assert abs(weights[0] - 1.0) < 0.0001
+    value = ind[0]
+    assert states == [value]
+    assert actions == [value]
+    assert rewards == [value]
+    assert next_states == [value]
+    assert dones == [value % 2]
+    assert log_probs == [value]
+    assert ind == [value]
+    assert abs(weights[0] - size) < 0.0001
+
+
+def test_sample_consecutive_values(memory_buffer_1e6):
+    for i in range(10):
+        memory_buffer_1e6.add(i, i, i, i, i % 2)
+
+    (
+        states_t1,
+        actions_t1,
+        rewards_t1,
+        next_states_t1,
+        dones_t1,
+        states_t2,
+        actions_t2,
+        rewards_t2,
+        next_states_t2,
+        dones_t2,
+        ind,
+    ) = memory_buffer_1e6.sample_consecutive(1)
+
+    value = ind[0]
+    assert states_t1 == [value]
+    assert actions_t1 == [value]
+    assert rewards_t1 == [value]
+    assert next_states_t1 == [value]
+    assert dones_t1 == [False]
+
+    assert states_t2 == [value + 1]
+    assert actions_t2 == [value + 1]
+    assert rewards_t2 == [value + 1]
+    assert next_states_t2 == [value + 1]
+    assert dones_t2 == [True]
+
+    assert ind == [value]
 
 
 def test_sample_more_than_buffer(memory_buffer):
@@ -174,4 +223,21 @@ def test_sample_empty_buffer(memory_buffer):
         )
     except ValueError:
         # Nothing in the buffer, so should receive not enough values to unpack
+        assert True
+
+    try:
+        (
+            states_t1,
+            actions_t1,
+            rewards_t1,
+            next_states_t1,
+            dones_t1,
+            states_t2,
+            actions_t2,
+            rewards_t2,
+            next_states_t2,
+            dones_t2,
+            ind,
+        ) = memory_buffer.sample_consecutive(1)
+    except ValueError:
         assert True
