3b176a2db50eaee2522e5363e89a7a89c0ae9a0b
beardyface <henryamwilliams@gmail.com>
2024-04-12 12:57:48 +1200

    start of consect

--- cares_reinforcement_learning/memory/prioritised_replay_buffer.py
+++ cares_reinforcement_learning/memory/prioritised_replay_buffer.py
@@ -1,5 +1,6 @@
+import random
+
 import numpy as np
-import torch
 
 from cares_reinforcement_learning.util.sum_tree import SumTree
 
@@ -220,44 +221,68 @@ class PrioritizedReplayBuffer:
         return experiences
 
     def sample_consecutive(self, batch_size):
-        # max_length = len(self.buffer) - 1
-        # candi_indices = list(range(max_length))
-        # batch_size = min(batch_size, max_length)
-        # # A list of candidate indices includes all indices.
-        # sampled_indices = []  # randomly sampled indices that is okay.
-        # # In this way, the sampling time depends on the batch size rather than buffer size.
-        # first_sample = True  # Not check duplicate for first time sample.
-        # while True:
-        #     # Sample size based on how many still needed.
-        #     idxs = np.random.randint(candi_indices, batch_size - len(sampled_indices))
-        #     for i in idxs:
-        #         # Check if it is already sampled.
-        #         already_sampled = False
-        #         # Only check if it is not first time in the while loop.
-        #         if not first_sample:
-        #             # compare with each item in the sampled.
-        #             for j in sampled_indices:
-        #                 if j == i:
-        #                     already_sampled = True
-        #         if (self.buffer[i][4] is False) and (not already_sampled):
-        #             sampled_indices.append(i)
-        #         if len(sampled_indices) == batch_size:
-        #             break
-        #     first_sample = False
-        #     if len(sampled_indices) == batch_size:
-        #         break
-        # # Form the sampled data batch
-        # experience_batch = [
-        #     self.buffer[i]
-        #     + (
-        #         self.buffer[i + 1][1],
-        #         self.buffer[i + 1][2],
-        #     )
-        #     for i in sampled_indices
-        # ]
-        # transposed_batch = zip(*experience_batch)
-        # return transposed_batch
-        return []
+        # If batch size is greater than size we need to limit it to just the data that exists
+        batch_size = min(batch_size, self.size)
+
+        candididate_indices = list(range(self.size))
+
+        # A list of candidate indices includes all indices.
+        sampled_indices = []  # randomly sampled indices that is okay.
+        # In this way, the sampling time depends on the batch size rather than buffer size.
+        first_sample = True  # Not check duplicate for first time sample.
+        while len(sampled_indices) < batch_size:
+            # Sample size based on how many still needed.
+            idxs = random.sample(candididate_indices, batch_size - len(sampled_indices))
+            for i in idxs:
+                # Check if it is already sampled.
+                already_sampled = False
+                # Only check if it is not first time in the while loop.
+                if not first_sample:
+                    # compare with each item in the sampled.
+                    for j in sampled_indices:
+                        if j == i:
+                            already_sampled = True
+                if (self.memory_buffers[4][i] is False) and (not already_sampled):
+                    sampled_indices.append(i)
+
+            first_sample = False
+
+        experiences = []
+        for buffer in self.memory_buffers:
+            # NOTE: we convert back to a standard list here
+            experiences.append(buffer[np.array(sampled_indices)].tolist())
+
+        next_sampled_indices = [x + 1 for x in sampled_indices]
+        next_sampled_indices = np.array(next_sampled_indices)
+        experiences.append(self.memory_buffers[1][next_sampled_indices].tolist())
+        experiences.append(self.memory_buffers[2][next_sampled_indices].tolist())
+        return experiences
+
+    def get_statistics(self):
+        """
+        Compute the statisitics for world model state normalization.
+        state, action, reward, next_state, done
+        Assumed Add sequence: (State, action, reward, next_state, next_action, next_reard)
+        :return: statistic tuple of the collected transitions.
+        """
+
+        states = np.array(self.memory_buffers[0][: self.ptr].tolist())
+        next_states = np.array(self.memory_buffers[3][: self.ptr].tolist())
+        delta = next_states - states
+
+        # Add a small number to avoid zeros.
+        observation_mean = np.mean(states, axis=0) + 0.00001
+        observation_std = np.std(states, axis=0) + 0.00001
+        delta_mean = np.mean(delta, axis=0) + 0.00001
+        delta_std = np.std(delta, axis=0) + 0.00001
+
+        statistics = {
+            "observation_mean": observation_mean,
+            "observation_std": observation_std,
+            "delta_mean": delta_mean,
+            "delta_std": delta_std,
+        }
+        return statistics
 
     def clear(self):
         """
