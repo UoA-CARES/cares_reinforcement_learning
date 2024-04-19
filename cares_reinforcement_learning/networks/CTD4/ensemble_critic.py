1444471e37b56677194a4447a351c0e166342f0b
beardyface <henryamwilliams@gmail.com>
2024-04-19 13:12:24 +1200

    EnsembleCritic for CTD4

--- cares_reinforcement_learning/networks/CTD4/ensemble_critic.py
+++ cares_reinforcement_learning/networks/CTD4/ensemble_critic.py
@@ -1,15 +1,16 @@
-import torch
 from torch import nn
 
 from cares_reinforcement_learning.networks.CTD4 import DistributedCritic as Critic
 
 
-class EnsembleCritic(torch.nn.ModuleList):
+class EnsembleCritic(nn.ModuleList):
     def __init__(self, ensemble_size, observation_size, action_num):
         super().__init__()
         self.ensemble_size = ensemble_size
 
-        critics = [Critic(observation_size, action_num) for _ in range(ensemble_size)]
+        critics = [
+            Critic(observation_size, action_num) for _ in range(self.ensemble_size)
+        ]
         self.extend(critics)
 
     def forward(self, state, action):
