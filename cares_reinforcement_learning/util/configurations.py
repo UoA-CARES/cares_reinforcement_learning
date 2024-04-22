05c724a270e60b5335ab63d74941dc9101330394
beardyface <henryamwilliams@gmail.com>
2024-04-22 14:04:19 +1200

    Fixed REDQ default G

--- cares_reinforcement_learning/util/configurations.py
+++ cares_reinforcement_learning/util/configurations.py
@@ -260,6 +260,8 @@ class REDQConfig(AlgorithmConfig):
     ensemble_size: Optional[int] = 10
     num_sample_critics: Optional[int] = 2
 
+    G: Optional[int] = 20
+
 
 class PERSACConfig(AlgorithmConfig):
     algorithm: str = Field("PERSAC", Literal=True)
