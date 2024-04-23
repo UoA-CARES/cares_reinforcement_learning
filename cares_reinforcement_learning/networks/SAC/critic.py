1e32faddf13166db90356fd80c5d9919334b4b07
rainingx683 <yxin683@aucklanduni.ac.nz>
2024-04-24 11:03:55 +1200

    refactor: sac network

--- cares_reinforcement_learning/networks/SAC/critic.py
+++ cares_reinforcement_learning/networks/SAC/critic.py
@@ -10,28 +10,27 @@ class Critic(nn.Module):
         self.hidden_size = [256, 256]
 
         # Q1 architecture
-        self.h_linear_1 = nn.Linear(observation_size + num_actions, self.hidden_size[0])
-        self.h_linear_2 = nn.Linear(self.hidden_size[0], self.hidden_size[1])
-        self.h_linear_3 = nn.Linear(self.hidden_size[1], 1)
+        self.Q1 = nn.Sequential(
+            nn.Linear(observation_size + num_actions, self.hidden_size[0]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[0], self.hidden_size[1]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[1], 1)
+        )
 
         # Q2 architecture
-        self.h_linear_12 = nn.Linear(
-            observation_size + num_actions, self.hidden_size[0]
+        self.Q2 = nn.Sequential(
+            nn.Linear(observation_size + num_actions, self.hidden_size[0]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[0], self.hidden_size[1]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[1], 1)
         )
-        self.h_linear_22 = nn.Linear(self.hidden_size[0], self.hidden_size[1])
-        self.h_linear_32 = nn.Linear(self.hidden_size[1], 1)
 
     def forward(
         self, state: torch.Tensor, action: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor]:
         obs_action = torch.cat([state, action], dim=1)
-
-        q1 = F.relu(self.h_linear_1(obs_action))
-        q1 = F.relu(self.h_linear_2(q1))
-        q1 = self.h_linear_3(q1)
-
-        q2 = F.relu(self.h_linear_12(obs_action))
-        q2 = F.relu(self.h_linear_22(q2))
-        q2 = self.h_linear_32(q2)
-
+        q1 = self.Q1(obs_action)
+        q2 = self.Q2(obs_action)
         return q1, q2
