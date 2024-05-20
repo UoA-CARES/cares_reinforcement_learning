4b183d13ffea181cb8d12d508b2f49929137902d
beardyface <henryamwilliams@gmail.com>
2024-05-21 08:11:08 +1200

    Actors for TD3

--- cares_reinforcement_learning/networks/RDTD3/actor.py
+++ cares_reinforcement_learning/networks/RDTD3/actor.py
@@ -1,11 +1,6 @@
-from cares_reinforcement_learning.networks.TD3 import Actor as TD3Actor
+"""
+This is a stub file for the Actor class - reads directly off SAC's Actor class.
+"""
 
-
-class Actor(TD3Actor):
-    def __init__(
-        self,
-        observation_size: int,
-        num_actions: int,
-        hidden_size: list[int] = None,
-    ):
-        super().__init__(observation_size, num_actions, hidden_size)
+# pylint: disable=unused-import
+from cares_reinforcement_learning.networks.TD3 import Actor
--- cares_reinforcement_learning/networks/TD3/actor.py
+++ cares_reinforcement_learning/networks/TD3/actor.py
@@ -1,13 +1,6 @@
-from cares_reinforcement_learning.networks.DDPG.actor import Actor as DDPGActor
+"""
+This is a stub file for the Actor class - reads directly off SAC's Actor class.
+"""
 
-
-class Actor(DDPGActor):
-    def __init__(
-        self,
-        observation_size: int,
-        num_actions: int,
-        hidden_size: list[int] = None,
-    ):
-        if hidden_size is None:
-            hidden_size = [256, 256]
-        super().__init__(observation_size, num_actions, hidden_size)
+# pylint: disable=unused-import
+from cares_reinforcement_learning.networks.DDPG.actor import Actor
