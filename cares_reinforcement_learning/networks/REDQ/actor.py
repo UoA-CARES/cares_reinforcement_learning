aaf9dda2411ab748e23fe8a62aefe5b2d6f9c7fa
beardyface <henryamwilliams@gmail.com>
2024-05-21 08:09:31 +1200

    Actors for SAC

--- cares_reinforcement_learning/networks/REDQ/actor.py
+++ cares_reinforcement_learning/networks/REDQ/actor.py
@@ -1,12 +1,6 @@
-from cares_reinforcement_learning.networks.SAC import Actor as SACActor
+"""
+This is a stub file for the Actor class - reads directly off SAC's Actor class.
+"""
 
-
-class Actor(SACActor):
-    def __init__(
-        self,
-        observation_size: int,
-        num_actions: int,
-        hidden_size: list[int] = None,
-        log_std_bounds: list[int] = None,
-    ):
-        super().__init__(observation_size, num_actions, hidden_size, log_std_bounds)
+# pylint: disable=unused-import
+from cares_reinforcement_learning.networks.SAC import Actor
