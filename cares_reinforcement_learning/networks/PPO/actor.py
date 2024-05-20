9957f5029da3e26d25ce7a0dff70da8208219741
beardyface <henryamwilliams@gmail.com>
2024-05-20 12:55:37 +1200

    Normalised DDPG and PPO

--- cares_reinforcement_learning/networks/PPO/actor.py
+++ cares_reinforcement_learning/networks/PPO/actor.py
@@ -1,26 +1,29 @@
 import torch
-import torch.nn.functional as F
 from torch import nn
 
 
 class Actor(nn.Module):
-    def __init__(self, observation_size: int, num_actions: int):
+    def __init__(
+        self,
+        observation_size: int,
+        num_actions: int,
+        hidden_size: list[int] = None,
+    ):
         super().__init__()
+        if hidden_size is None:
+            hidden_size = [1024, 1024]
 
-        self.hidden_size = [1024, 1024]
+        self.hidden_size = hidden_size
 
-        self.h_linear_1 = nn.Linear(
-            in_features=observation_size, out_features=self.hidden_size[0]
-        )
-        self.h_linear_2 = nn.Linear(
-            in_features=self.hidden_size[0], out_features=self.hidden_size[1]
-        )
-        self.h_linear_3 = nn.Linear(
-            in_features=self.hidden_size[1], out_features=num_actions
+        self.act_net = nn.Sequential(
+            nn.Linear(observation_size, self.hidden_size[0]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[0], self.hidden_size[1]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size[1], num_actions),
+            nn.Tanh(),
         )
 
     def forward(self, state: torch.Tensor) -> torch.Tensor:
-        x = F.relu(self.h_linear_1(state))
-        x = F.relu(self.h_linear_2(x))
-        x = torch.tanh(self.h_linear_3(x))
-        return x
+        output = self.act_net(state)
+        return output
