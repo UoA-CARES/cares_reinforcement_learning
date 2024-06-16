c5badbaa66008ad2720a4abd8419f02af37e7277
beardyface <henryamwilliams@gmail.com>
2024-06-17 08:15:04 +1200

    Dynamically sized Burg autoencoder

--- cares_reinforcement_learning/networks/encoders/burgess_autoencoder.py
+++ cares_reinforcement_learning/networks/encoders/burgess_autoencoder.py
@@ -1,3 +1,8 @@
+"""
+
+Original Code: https://github.com/YannDubs/disentangling-vae/tree/master
+"""
+
 import numpy as np
 import torch
 from torch import nn
@@ -48,13 +53,16 @@ class BurgessAutoencoder(nn.Module):
         no_discrim=False,
         is_agent=False,
     ):
-        states = states / 255
+        # states = states / 255
 
-        latent_dist = self.encoder(states, detach)
+        mean, logvar = self.encoder(states, detach)
+        print(f"{mean.shape=}")
 
-        latent_sample = self.reparameterize(*latent_dist)
+        latent_sample = self.reparameterize(mean, logvar)
+        print(f"{latent_sample.shape=}")
 
         rec_obs = self.decoder(latent_sample)
+        print(f"{rec_obs.shape=}")
         loss = 0
 
         # loss = self.loss_fn(
@@ -75,7 +83,7 @@ class BurgessAutoencoder(nn.Module):
         return {
             "loss": loss,
             "z_vector": latent_sample,
-            "latent_dist": latent_dist,
+            "latent_dist": [mean, logvar],
             "rec_obs": rec_obs,
         }
 
@@ -101,66 +109,92 @@ class BurgessEncoder(nn.Module):
         self.num_layers = num_layers
         self.num_filters = num_filters
         self.kernel_size = kernel_size
+        self.hidden_dim = hidden_dim
 
+        stride = 2
+        padding = 0
         self.convs = nn.ModuleList(
             [
                 nn.Conv2d(
                     observation_size[0],
                     self.num_filters,
                     kernel_size=self.kernel_size,
-                    stride=2,
-                    padding=1,
+                    stride=stride,
+                    padding=padding,
                 )
             ]
         )
 
-        self.out_dim = hlp.flatten(observation_size[1], k=self.kernel_size, s=2, p=1)
+        self.out_dim = hlp.flatten(
+            observation_size[1], k=self.kernel_size, s=stride, p=padding
+        )
 
+        stride = 1
+        padding = 0
         for _ in range(self.num_layers - 1):
             self.convs.append(
                 nn.Conv2d(
                     self.num_filters,
                     self.num_filters,
                     kernel_size=self.kernel_size,
-                    stride=2,
-                    padding=1,
+                    stride=stride,
+                    padding=padding,
                 )
             )
-            self.out_dim = hlp.flatten(self.out_dim, k=self.kernel_size, s=2, p=1)
+            self.out_dim = hlp.flatten(
+                self.out_dim, k=self.kernel_size, s=stride, p=padding
+            )
 
         self.n_flatten = self.out_dim * self.out_dim * self.num_filters
 
         # Fully connected layers
-        self.linear_one = nn.Linear(self.n_flatten, hidden_dim)
-        self.linear_two = nn.Linear(hidden_dim, hidden_dim)
+        self.linear_one = nn.Linear(self.n_flatten, self.hidden_dim)
+        self.linear_two = nn.Linear(self.hidden_dim, self.hidden_dim)
 
         # Fully connected layers for mean and variance
-        self.mu_logvar_gen = nn.Linear(hidden_dim, self.latent_dim * 2)
+        self.mu_logvar_gen = nn.Linear(self.hidden_dim, self.latent_dim * 2)
 
     def _forward_conv(self, x: torch.Tensor) -> torch.Tensor:
         conv = torch.relu(self.convs[0](x))
+        print(f"{conv.shape=}")
+
         for i in range(1, self.num_layers):
             conv = torch.relu(self.convs[i](conv))
+            print(f"{conv.shape=}")
+
         return conv
 
-    def enc_forward(self, x, detach=False):
-        batch_size = x.size(0)
+    def enc_forward(
+        self, obs: torch.Tensor, detach_cnn: bool = False, detach_output: bool = False
+    ):
+        batch_size = obs.size(0)
 
         # Convolutional layers with ReLu activations
-        x = self._forward_conv(x)
-        print(f"{x.shape=}")
+        print(f"{obs.shape=}")
+        h = self._forward_conv(obs)
+
+        # SAC AE detaches at the CNN layer
+        if detach_cnn:
+            h = h.detach()
 
         # Fully connected layers with ReLu activations
-        x = x.view((batch_size, -1))
-        x = torch.relu(self.linear_one(x))
-        x = torch.relu(self.linear_two(x))
+        h = h.view((batch_size, -1))
+        print(f"{h.shape=}")
+        h = torch.relu(self.linear_one(h))
+        print(f"{h.shape=}")
+        h = torch.relu(self.linear_two(h))
+        print(f"{h.shape=}")
 
         # Fully connected layer for log variance and mean
         # Log std-dev in paper (bear in mind)
-        mu_logvar = self.mu_logvar_gen(x)
-        mu, logvar = mu_logvar.view(-1, self.latent_dim, 2).unbind(-1)
+        mu_logvar = self.mu_logvar_gen(h)
+        print(f"{mu_logvar.shape=}")
 
-        if detach:
+        mu, logvar = mu_logvar.view(-1, self.latent_dim, 2).unbind(-1)
+        print(f"{mu.shape=}")
+        print(f"{logvar.shape=}")
+
+        if detach_output:
             mu = mu.detach()
             logvar = logvar.detach()
 
@@ -179,73 +213,74 @@ class BurgessDecoder(nn.Module):
         out_dim: int,
         num_layers: int = 4,
         num_filters: int = 32,
-        kernel_size: int = 3,
+        kernel_size: int = 4,
+        hidden_dim: int = 256,
     ):
         super().__init__()
 
-        # Layer parameters
-        hid_channels = 32
-        kernel_size = 4
-        hidden_dim = 256
-        self.img_size = observation_size
+        self.latent_dim = latent_dim
+        self.out_dim = out_dim
 
-        # Shape required to start transpose convs
-        self.reshape = (hid_channels, kernel_size, kernel_size)
-        n_chan = self.img_size[0]
-        self.img_size = observation_size
+        self.num_layers = num_layers
+        self.num_filters = num_filters
+        self.kernel_size = kernel_size
+        self.hidden_dim = hidden_dim
+
+        self.n_flatten = self.out_dim * self.out_dim * self.num_filters
 
         # Fully connected layers
-        self.lin1 = nn.Linear(latent_dim, hidden_dim)
-        self.lin2 = nn.Linear(hidden_dim, hidden_dim)
-        self.lin3 = nn.Linear(hidden_dim, np.prod(self.reshape))
+        self.linear_one = nn.Linear(latent_dim, hidden_dim)
+        self.linear_two = nn.Linear(hidden_dim, hidden_dim)
+        self.linear_three = nn.Linear(hidden_dim, self.n_flatten)
 
-        # Convolutional layers
-        cnn_kwargs = dict(stride=2, padding=1)
-        # If input image is 64x64 do fourth convolution
-        if self.img_size[1] == self.img_size[2] == 64:
-            self.convT_64 = nn.ConvTranspose2d(
-                hid_channels, hid_channels, kernel_size, **cnn_kwargs
+        self.deconvs = nn.ModuleList()
+
+        stride = 1
+        padding = 0
+        for _ in range(self.num_layers - 1):
+            self.deconvs.append(
+                nn.ConvTranspose2d(
+                    in_channels=self.num_filters,
+                    out_channels=self.num_filters,
+                    kernel_size=self.kernel_size,
+                    stride=stride,
+                    padding=padding,
+                )
             )
 
-        self.convT1 = nn.ConvTranspose2d(
-            hid_channels, hid_channels, kernel_size, **cnn_kwargs
-        )
-        self.convT2 = nn.ConvTranspose2d(
-            hid_channels, hid_channels, kernel_size, **cnn_kwargs
-        )
-        self.convT3 = nn.ConvTranspose2d(
-            hid_channels, n_chan, kernel_size, **cnn_kwargs
+        stride = 2
+        padding = 1
+        self.deconvs.append(
+            nn.ConvTranspose2d(
+                in_channels=self.num_filters,
+                out_channels=observation_size[0],
+                kernel_size=self.kernel_size,
+                stride=stride,
+                output_padding=padding,
+            )
         )
 
-    def forward(self, z):
-        batch_size = z.size(0)
+    def forward(self, latent_obs):
+        batch_size = latent_obs.size(0)
 
         # Fully connected layers with ReLu activations
-        x = torch.relu(self.lin1(z))
-        x = torch.relu(self.lin2(x))
-        x = torch.relu(self.lin3(x))
-        x = x.view(batch_size, *self.reshape)
+        h_fc = torch.relu(self.linear_one(latent_obs))
+        print(f"{h_fc.shape=}")
+        h_fc = torch.relu(self.linear_two(h_fc))
+        print(f"{h_fc.shape=}")
+        h_fc = torch.relu(self.linear_three(h_fc))
+        print(f"{h_fc.shape=}")
+
+        deconv = h_fc.view(batch_size, self.num_filters, self.out_dim, self.out_dim)
+        print(f"{deconv.shape=}")
 
         # Convolutional layers with ReLu activations
-        if self.img_size[1] == self.img_size[2] == 64:
-            x = torch.relu(self.convT_64(x))
-        x = torch.relu(self.convT1(x))
-        x = torch.relu(self.convT2(x))
+        for i in range(0, self.num_layers - 1):
+            deconv = torch.relu(self.deconvs[i](deconv))
+            print(f"{deconv.shape=}")
+
         # Sigmoid activation for final conv layer
-        x = torch.sigmoid(self.convT3(x))
+        observation = torch.sigmoid(self.deconvs[-1](deconv))
+        print(f"{observation.shape=}")
 
-        return x
-
-
-# TODO Remove here before final pull request
-
-
-def main():
-    ae = BurgessAutoencoder((3, 64, 64), 10, num_filters=16, kernel_size=4)
-
-    x = torch.randn(1, 3, 64, 64)
-    out = ae(x)
-
-
-if __name__ == "__main__":
-    main()
+        return observation
