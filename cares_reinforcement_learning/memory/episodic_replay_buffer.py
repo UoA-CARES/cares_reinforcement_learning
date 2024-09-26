d91c26d60711279339e1d42026af6cdb99177d68
hoda <hyam650@aucklanduni.ac.nz>
2024-09-27 09:31:51 +1200

    All long-term for mix-reward

--- cares_reinforcement_learning/memory/episodic_replay_buffer.py
+++ cares_reinforcement_learning/memory/episodic_replay_buffer.py
@@ -2,6 +2,7 @@
 
 from cares_reinforcement_learning.memory.short_term_replay_buffer import ShortTermReplayBuffer
 from cares_reinforcement_learning.memory.long_memory_buffer import LongMemoryBuffer
+from cares_reinforcement_learning.memory.long_memory_buffer import LongMemoryBufferTotal
 from cares_reinforcement_learning.memory.episodic_buffer import EpisodicBuffer
 
 
@@ -13,5 +14,6 @@ class ManageBuffers:
         
         self.short_term_memory = ShortTermReplayBuffer(max_capacity=max_capacity, **memory_params)
         self.long_term_memory = LongMemoryBuffer(max_capacity=int(10), **memory_params)
+        self.long_term_memory_total = LongMemoryBufferTotal(max_capacity=int(10), **memory_params)
         #self.episodic_memory = EpisodicBuffer(max_capacity=max_capacity)
         
