3d2ad3d7831bdc3c0b2959356e98213ac580fbdd
beardyface <henryamwilliams@gmail.com>
2025-03-21 18:28:09 +1300

    Fix test

--- tests/memory/test_add.py
+++ tests/memory/test_add.py
@@ -1,5 +1,5 @@
 import timeit
-from memory import memory_buffer, memory_buffer_n_step
+from memory import memory_buffer, memory_buffer_1e6
 
 
 def test_add(memory_buffer):
@@ -22,19 +22,19 @@ def test_overfill_buffer(memory_buffer):
     assert len(memory_buffer) == 5
 
 
-def test_consistant_insertion_time(memory_buffer_n_step):
+def test_consistant_insertion_time(memory_buffer_1e6):
 
     first_insertion = timeit.timeit(
-        lambda: memory_buffer_n_step.add(1, 2, 3, 4, False, 0.5), number=1
+        lambda: memory_buffer_1e6.add(1, 2, 3, 4, False, 0.5), number=1
     )
 
     for i in range(1000000):
-        memory_buffer_n_step.add(i, i, i, i, False, 0.5 * i)
+        memory_buffer_1e6.add(i, i, i, i, False, 0.5 * i)
 
-    assert len(memory_buffer_n_step) == 1e6
+    assert len(memory_buffer_1e6) == 1e6
 
     last_insertion = timeit.timeit(
-        lambda: memory_buffer_n_step.add(1, 2, 3, 4, False, 0.5), number=1
+        lambda: memory_buffer_1e6.add(1, 2, 3, 4, False, 0.5), number=1
     )
 
     assert last_insertion < 10 * first_insertion
