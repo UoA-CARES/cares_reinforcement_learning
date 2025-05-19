1784b14b089b876a316183b3aeca0f295fb44ab4
Sam Boasman <samboasman@gmail.com>
2025-05-20 10:51:32 +1200

    Update SAC twinQ critic to utilise a 2-layer residual block

--- cares_reinforcement_learning/networks/SAC/critic.py
+++ cares_reinforcement_learning/networks/SAC/critic.py
@@ -1,4 +1,4 @@
-from torch import nn
+from torch import nn, Tensor
 
 from cares_reinforcement_learning.networks.common import TwinQNetwork, BaseCritic
 from cares_reinforcement_learning.util.configurations import SACConfig
@@ -46,10 +46,51 @@ class DefaultCritic(TwinQNetwork):
         )
 
 
+class ResidualMLPBlock(nn.Module):
+    def __init__(self, size: int, stride: int = 2):
+        super().__init__()
+
+        if stride < 2:
+            raise ValueError("Stride must be greater than or equal to 2")
+
+        self.resblock = nn.Sequential(
+            nn.Linear(size, size),
+            nn.ReLU(),
+            nn.Linear(size, size),
+        )
+
+        for _ in range(stride - 2):
+            self.resblock.append(nn.ReLU())
+            self.resblock.append(nn.Linear(size, size))
+
+        self.activation = nn.ReLU()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.activation(self.resblock(x) + x)
+
+
 class Critic(TwinQNetwork):
     def __init__(self, observation_size: int, num_actions: int, config: SACConfig):
         input_size = observation_size + num_actions
 
-        super().__init__(
-            input_size=input_size, output_size=1, config=config.critic_config
+        hidden_size = 1024
+
+        super().__init__(input_size=input_size, output_size=1, config=config.critic_config)
+
+        # Q1 architecture
+        # pylint: disable-next=invalid-name
+        self.Q1 = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            ResidualMLPBlock(hidden_size),
+            nn.Linear(hidden_size, 1),
+        )
+
+        # Q2 architecture
+        # pylint: disable-next=invalid-name
+        self.Q2 = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            ResidualMLPBlock(hidden_size),
+            nn.Linear(hidden_size, 1),
         )
