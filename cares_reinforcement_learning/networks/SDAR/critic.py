b64d5e1c51f176c3edeb6b8664e13df344281d11
Henry Williams <HenryAMWilliams@gmail.com>
2025-06-03 15:56:13 +1200

    SDAR (#296)
    * SDAR

---
+++ cares_reinforcement_learning/networks/SDAR/critic.py
@@ -0,0 +1,6 @@
+"""
+This is a stub file for the Actor class - reads directly off SAC's Actor class.
+"""
+
+# pylint: disable=unused-import
+from cares_reinforcement_learning.networks.SAC import Critic, DefaultCritic
