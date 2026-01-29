9e218c5406dceeee5070c6f0b3f9be9faacc82aa
Sam Boasman <samboasman@gmail.com>
2026-01-29 17:29:13 +1300

    Fix SACD creation to handle vector state observation size

--- cares_reinforcement_learning/util/network_factory.py
+++ cares_reinforcement_learning/util/network_factory.py
@@ -414,6 +414,8 @@ def create_SACD(observation_size, action_num, config: acf.SACDConfig, num_tasks:
     if config.image_observation or isinstance(observation_size, tuple):
         input_size = config.autoencoder_config.latent_dim
         config.autoencoder_config.observation_size = observation_size
+    else:
+        input_size = observation_size
 
     actor = Actor(input_size, action_num, config=config)
     critic = Critic(input_size, action_num, config=config)
