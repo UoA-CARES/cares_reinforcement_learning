3901a07085f5c9f6c23e4f857458357cc0089312
rayishihara <rayishihara06@gmail.com>
2026-05-21 21:57:38 +1200

    feat: complete LagrangeMultiplier class

--- cares_reinforcement_learning/algorithm/configurations.py
+++ cares_reinforcement_learning/algorithm/configurations.py
@@ -838,7 +838,6 @@ class SACDConfig(AlgorithmConfig):
 
 class SACLagConfig(SACConfig):
     cost_gamma: float = 0.99
-    cost_limit: float = 25.0
 
     cost_critic_lr: float = 3e-4
     cost_critic_lr_params: dict[str, Any] = Field(default_factory=dict)
@@ -848,17 +847,18 @@ class SACLagConfig(SACConfig):
 
 class LagrangeMultiplierConfig(BaseModel):
     init: float = 0.01
+    cost_limit: float = 25
+    upper_bound: float | None = None
 
-    lr: float = 3e-4
+    lr: float = 1e-3  # also called step size
     lr_params: dict[str, Any] = Field(default_factory=dict)
 
     update_method: Literal["fixed", "gradient_ascent", "pid_controller"] = "fixed"
 
-    step_size: float = 1e-3
-
     pid_kp: float = 0.1
     pid_ki: float = 0.01
     pid_kd: float = 0.01
+    integral_max: float = 100
 
 
 ###################################
